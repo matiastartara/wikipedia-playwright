1f4e47a33cc5cae71c2a84ed714acf42c9fa129d
matiastartara <19376841+matiastartara@users.noreply.github.com>
2026-07-06 16:57:52 -0300

    Add ArticlePage class and update search test to validate article title

---
+++ pages/ArticlePage.ts
@@ -0,0 +1,15 @@
+import { Page, Locator } from 'playwright/test';
+
+export class ArticlePage {
+    readonly page: Page;
+    readonly articleTitle: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.articleTitle = page.locator('#firstHeading');
+    }
+
+    async getArticleTitle(): Promise<string> {
+        return this.articleTitle.innerText();
+    }
+}
--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,15 +1,20 @@
 import { test, expect } from '@playwright/test';
 import { LanguagePage } from '../pages/LanguagePage';
 import { HomePage } from '../pages/HomePage';
+import { ArticlePage } from '../pages/ArticlePage';
+
 
 test.describe('Wikipedia - Search test', () => {
   test('Search for a term', async ({ page }) => {
     const languagePage = new LanguagePage(page);
     const homePage = new HomePage(page);
+    const articlePage = new ArticlePage(page);
 
     await languagePage.goTo();
     await languagePage.selectLanguage('English')
     await homePage.search('Playwright (software)');
 
+    const articleTitle = await articlePage.getArticleTitle();
+    expect(articleTitle).toBe('Playwright (software)');
   });
 })
